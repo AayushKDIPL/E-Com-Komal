4b41ecbf899e0063a158e5138c659bc6e2a2b90c
AayushKDIPL <aayush@kanakdrishtiinfo.com>
2024-07-03 18:23:03 +0530

    Update Location.js

--- src/Contact/Location.js
+++ src/Contact/Location.js
@@ -3,7 +3,7 @@ const Location=()=>{
     return(
         <>
             <div className="container location-map">
-                <div className="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d112113.14831042667!2d77.23833489106983!3d28.583696305714234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1719902076097!5m2!1sen!2sin" width="100%" height="100%" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+                <div className="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d112113.14831042667!2d77.23833489106983!3d28.583696305714234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1719902076097!5m2!1sen!2sin" width="100%" height="100%" loading="lazy" title="Google Map" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
                 <div className="location mt-5">
                     <div className="location1">
                         <h4>Office</h4>
